1cc2449ae40066f95d8f1ac0b67584dcdbdf45c3
tick <895866818@qq.com>
2025-08-09 22:51:22 +0800

    优化digest比较

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -11,6 +11,18 @@ echo 用户信息 $(whoami)
 echo docker版本
 docker version | grep -i version
 
+install_cmd(){
+    type jq
+    if [ $? -ne 0 ];then
+        apt install -y jq
+    fi
+
+    type skopeo
+    if [ $? -ne 0 ];then
+        apt install -y skopeo
+    fi
+}
+
 fun_update(){
     echo "------------------分割线-------------------"
     IMAGE=$1
@@ -20,6 +32,21 @@ fun_update(){
     NEWIMAGE=$(echo $IMAGE | sed s#/#-# )
     #tag不变
     NEWTAG=$TAG
+    echo "check digest same or not"
+    docker.io/zerotier/zerotier:1.14.0
+    registry.cn-chengdu.aliyuncs.com/tickg/zerotier-zerotier
+
+    org_digest=$(skopeo inspect --tls-verify=false docker://docker.io/$IMAGE:$TAG | jq -r '.Digest')
+    ali_digest=$(skopeo inspect --tls-verify=false docker://$REGISTRY/$NAMESPACE/$NEWIMAGE:$NEWTAG | jq -r '.Digest')
+    echo org_digest=$org_digest
+    echo ali_digest=$ali_digest
+    if [[ "$org_digest" != "" && "$org_digest" != "$ali_digest" ]];then
+        echo "need update"
+    else
+        echo "same ignore update"
+        return
+    fi
+
     echo 开始拉取
     docker pull $IMAGE:$TAG  > tmp.log
     tail -3 tmp.log
@@ -38,24 +65,28 @@ fun_update(){
 
 
 do_main(){
+    install_cmd
+
+    #
     fun_update vaultwarden/server                 latest
-    fun_update portainer/portainer-ce             latest
-    fun_update nginx                              1.27
+    #fun_update portainer/portainer-ce             latest
+    #fun_update nginx                              1.27
     fun_update nginx                              latest
-    fun_update jc21/nginx-proxy-manager           latest
+    #fun_update jc21/nginx-proxy-manager           latest
     # v1.5.9 , 9个月了一直没更新了
-    fun_update productiveops/dokemon              latest
+    #fun_update productiveops/dokemon              latest
     # v2.4 ，  6年一直没更新了
-    fun_update bytemark/webdav                    latest
+    #fun_update bytemark/webdav                    latest
     #
     fun_update zerotier/zerotier                  latest
     fun_update zerotier/zerotier                  1.14.0
     #
-    fun_update louislam/dockge                    1
-    fun_update louislam/dockge                    latest
+    #fun_update louislam/dockge                    1
+    #fun_update louislam/dockge                    latest
     #
     
-}
+
+}# 
 
 do_main
 
